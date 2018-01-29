ed0722a0d872bb679314535552aebf13873b4670
tassio <tassionoronha@gmail.com>
2018-01-29 11:03:32 +0100

    commit initial

---
+++ Classes.js
@@ -0,0 +1,60 @@
+class Radar{
+  constructor(year, objHTML){
+    this.year = year;
+    this.objHTML = objHTML;
+    this.init = (this.year - 2010) * 12;
+    this.end = this.init + 11;
+    this.data = Object.values(dataMonth[0].data);
+    this.values = [];
+    this.chart = null;
+  }
+
+  generateDynamicArrayMonth(){
+    for (var i = this.init; i <= this.end; i++) {
+      this.values[i] = this.data[i];
+    }
+  }
+
+  generateChart(){
+    this.values = [];
+    this.generateDynamicArrayMonth();
+    this.chart = new Chart(this.objHTML, {
+      type: 'radar',
+      data: {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        datasets: [{
+          label: 'Grenoble Pollution',
+          backgroundColor: "rgba(153,255,51,0.4)",
+          borderColor: "rgba(153,255,51,1)",
+          data: this.values
+        }]
+      }
+    });
+  }
+}
+
+class Lignes{
+
+  constructor(objHTML){
+    this.objHTML = objHTML;
+    this.keys = Object.keys(dataChart[0].data);
+    this.values = Object.values(dataChart[0].data);
+    this.chart = null;
+  }
+
+  generateChart(){
+    this.chart = new Chart(this.objHTML, {
+      type: 'line',
+      data: {
+        labels: this.keys,
+        datasets: [{
+          label: 'Pollution',
+          data: this.values,
+          backgroundColor: "rgba(153,255,51,0.4)"
+        }]
+      }
+    });
+  }
+
+
+}
