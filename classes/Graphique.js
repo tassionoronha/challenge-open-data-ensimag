8b1ebae0f419df14574b7eec4b503bebff503b37
gf101010 <>
2018-01-31 11:04:25 +0100

    Change floor on color scale creation

--- classes/Graphique.js
+++ classes/Graphique.js
@@ -26,9 +26,9 @@ class Graphique {
 
   getColorScale(nbColors, RGB){
     let colors = [];
-    let step = Math.floor(255/nbColors);
+    let step = 255/nbColors;
     for (let i = 0; i < nbColors; ++i) {
-      let value = step + i * step;
+      let value = Math.floor(step + i * step);
       switch (RGB) {
       case 0:
         colors[i] = {r:value, g:0 , b:127};
